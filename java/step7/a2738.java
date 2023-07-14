ac62e8a779a30ce77e803a15fc3b8fe37d9c1525
NoRuTnT <moonabcd@naver.com>
2023-07-14 23:31:06 +0900

    23.07.14

---
+++ java/step7/a2738.java
@@ -0,0 +1,24 @@
+package com.example.tflite.java.step5;
+import java.util.Scanner;
+
+public class a2738 {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(), M = sc.nextInt();
+
+        int[][] A = new int[N][M];
+
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<M; j++) {
+                A[i][j] = sc.nextInt();
+            }
+        }
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<M; j++) {
+                A[i][j] += sc.nextInt();
+                System.out.print(A[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
